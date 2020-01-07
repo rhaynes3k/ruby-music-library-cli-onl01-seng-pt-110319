4b42b0e451a8ebe2fd735ec8d0a00df65e9d8b32
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-07 22:23:16 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -17,7 +17,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-    #   command = gets.chomp
+       command = gets.chomp
     # end
     # if command == "list songs"
     #   list_songs
